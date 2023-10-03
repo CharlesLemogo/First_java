f7c72070200aff7c38cceba711d33c4c058a7bae
Charles Lemogo <your_name@example.com>
2023-10-03 19:13:26 +0100

    first commit

---
+++ First.java
@@ -0,0 +1,7 @@
+public class First {
+	public static void main(String[] args) {
+		System.out.println("My name is Lemogo Charles");
+		System.out.println("I am 24 years old");
+		System.out.println("My home town is Douala, Cameroon");
+	}
+}
